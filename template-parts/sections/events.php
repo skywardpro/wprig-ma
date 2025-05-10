1e30e2c5fdff6d71bd9b9b345d3ab1c2e7a7fb61
Valery Pronin <pownmymail@gmail.com>
2025-05-10 20:24:19 +0300

    refactor: switch .column.is-... to w-... classes in Events Section

--- template-parts/sections/events.php
+++ template-parts/sections/events.php
@@ -11,26 +11,24 @@ namespace WP_Rig\WP_Rig;
 ?>
 <section id="events" class="events mb-6xl mb-5xl__desktop bg-color--gradient py-7xl py-5xl__tablet py-7xl__desktop">
 	<div class="container py-6xl__widescreen">
-		<div class="columns">
-			<div class="column is-8-tablet is-6-desktop">
-				<!-- Heading -->
-				<h2 class="color--white typo--tag-small typo--tag-big__desktop typo--regular mt-0 mb-xs mb-l__tablet mb-m__desktop">
-					[&nbsp;&nbsp;Events&nbsp;&nbsp;]
-				</h2>
+		<div class="w-75__tablet w-50__widescreen">
+			<!-- Heading -->
+			<h2 class="color--white typo--tag-small typo--tag-big__desktop typo--regular mt-0 mb-xs mb-l__tablet mb-m__desktop">
+				[&nbsp;&nbsp;Events&nbsp;&nbsp;]
+			</h2>
 
-				<!-- Subheading -->
-				<p class="color--white typo--h3 typo--unbounded typo--light mt-0 mb-xs mb-l__tablet mb-m__desktop typo--h1__tablet">
-					YOUR EVENT, <br />OUR CONNECTIONS
-				</p>
+			<!-- Subheading -->
+			<p class="color--white typo--h3 typo--unbounded typo--light mt-0 mb-xs mb-l__tablet mb-m__desktop typo--h1__tablet">
+				YOUR EVENT, <br />OUR CONNECTIONS
+			</p>
 
-				<!-- Caption -->
-				<p class="color--white typo--body-big typo--medium mt-0 mb-5xl mb-4xl__tablet mb-5x__widescreen typo--subtitle-big__tablet typo--semibold__tablet">
-					MarketAcross is the media partner for the world’s leading crypto and blockchain events
-				</p>
+			<!-- Caption -->
+			<p class="color--white typo--body-big typo--medium mt-0 mb-5xl mb-4xl__tablet mb-5x__widescreen typo--subtitle-big__tablet typo--semibold__tablet">
+				MarketAcross is the media partner for the world’s leading crypto and blockchain events
+			</p>
 
-				<!-- CTA Button -->
-				<a href="#" class="btn--white is-full-mobile">More About Events</a>
-			</div>
+			<!-- CTA Button -->
+			<a href="#" class="btn--white is-full-mobile">More About Events</a>
 		</div>
 	</div>
 </section>
