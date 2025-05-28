a2461571dd8ea22a29c708a5d56de962455148ae
Valery Pronin <pownmymail@gmail.com>
2025-05-29 00:12:37 +0300

    feat: layout The Services Cards content

--- template-parts/sections/the-services.php
+++ template-parts/sections/the-services.php
@@ -30,90 +30,207 @@ namespace WP_Rig\WP_Rig;
 			<mark class="bg-color--purple-30">Messaging strategy, pitching journalists, being immersed in the industry - we don't count these as services.</mark> These are the things we do internally in order to be able to deliver on the services you hired us for.
 		</p>
 
-		<?php
-		$dummy_card = array(
-			'title' => 'Hello World',
-			'subtitle' => 'Yeh! We know that we best!',
-			'text' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>Exercitationem sequi ducimus, dolore, doloremque quis ut culpa perferendis voluptatibus natus numquam consectetur quasi corporis ea blanditiis iusto sunt laborum, recusandae consequuntur in velit! Voluptatibus, fugiat. <br>Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
-			'tags' => array(
-				'op-eds',
-				'podcasts',
-				'expert quotes',
-				'data citations',
-				'commentaries',
-				'interviews',
-				'newsjacking'
-			),
-			'image_url' => get_stylesheet_directory_uri() . '/assets/images/the-services-slider-card-bg.webp'
-		);
-		?>
 		<!-- The Services Swiper -->
 		<div class="swiper the-services-swiper overflow--visible mt-0 mb-l mb-xl__tablet mb-4xl__widescreen overflow--hidden__widescreen">
 			<div class="swiper-wrapper align-items--stretch">
-				<?php
-				for ( $i = 0; $i < 5; $i++ ) {
-					?>
-					<!-- The Services Swiper Card -->
-					<div class="swiper-slide is-flex flex-direction--column the-services-swiper__slide is-rounded--4xs is-clipped bg-color--white p-xs color--white p-m__desktop p-xs__widescreen p-m__fullhd <?php echo $i === 0 ? 'active' : '' ?>">
-						<div class="the-services-swiper__slide-face is-flex h-100 flex-direction--column">
 
-							<!-- BgImage -->
-							<img width="320" height="526" src="<?php echo $dummy_card['image_url'] ?>" alt="<?php echo $dummy_card['title'] ?>" class="the-services-swiper__slide-bg">
+				<!-- The Services Swiper Card 1 -->
+				<div class="swiper-slide is-flex flex-direction--column the-services-swiper__slide is-rounded--4xs is-clipped bg-color--white p-xs color--white p-m__desktop p-xs__widescreen p-m__fullhd active">
+					<div class="the-services-swiper__slide-face is-flex h-100 flex-direction--column">
 
-							<!-- Title -->
-							<h3 class="typo--h1 typo--light mt-0 mb-2xs">
-								<?php echo $dummy_card['title'] ?>
-							</h3>
+						<!-- BgImage -->
+						<img width="320" height="526" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/the-services-slider-card-bg.webp' ?>" alt="Market Across PR" class="the-services-swiper__slide-bg">
 
-							<!-- Subtitle -->
-							<p class="typo--body-big my-0 overflow--hidden">
-								<?php echo $dummy_card['subtitle'] ?>
+						<!-- Title -->
+						<h3 class="typo--h1 typo--light mt-0 mb-2xs">
+							PR
+						</h3>
+
+						<!-- Subtitle -->
+						<p class="typo--body-big my-0 overflow--hidden">
+							Lorem ipsum dolor sit amet consectetur.
+						</p>
+
+						<!-- OpenBtn -->
+						<button class="the-services-swiper__slide-open reset-button-styles is-rounded--full mt-auto ml-auto flex-shrink--0 mr-xs mb-xs mr-m mr-m__desktop mb-m__desktop mr-xs__widescreen mr-xs_widescreen mr-m__fullhd mb-m__fullhd"></button>
+					</div>
+					<!-- Overlay -->
+					<div class="the-services-swiper__slide-overlay p-xs p-m__desktop p-xs__widescreen p-m__fullhd">
+						<div class="is-flex h-100 flex-direction--column">
+
+							<!-- Text -->
+							<p class="is-hidden-mobile the-services-swiper__slide-overlay-text typo--body typo--medium mt-0 mb-2xs">
+								We’ll say it again, we do things a bit differently. On three levels:
+							</p>
+							<p class="the-services-swiper__slide-overlay-text typo--body typo--medium mt-0 mb-2xs">
+								<span class="is-hidden-mobile">&nbsp;1. </span>We don’t just wait to get news from you to pitch. We go out there and create media opportunities for you. Be it expert quotes, commentary, podcasts or opinion pieces - we keep the ball rolling, always in the press court.
+							</p>
+							<p class="the-services-swiper__slide-overlay-text typo--body typo--medium mt-0 mb-2xs">
+								<span class="is-hidden-mobile">&nbsp;2. </span>For us Content Marketing is an integral part of the PR mix. Since we know that you (probably) don’t have a constant stream of news, we use content marketing to build up your brand and keep it in the conversation.
+							</p>
+							<p class="the-services-swiper__slide-overlay-text typo--body typo--medium mt-0 mb-2xs">
+								<span class="is-hidden-mobile">&nbsp;3. </span>After we land media coverage for you we keep at it, amplifying its exposure and impact so it will reach as wide an audience as possible.
 							</p>
 
-							<!-- OpenBtn -->
-							<button class="the-services-swiper__slide-open reset-button-styles is-rounded--full mt-auto ml-auto flex-shrink--0"></button>
-						</div>
-						<!-- Overlay -->
-						<div class="the-services-swiper__slide-overlay p-xs p-m__desktop p-xs__widescreen p-m__fullhd">
-							<div class="is-flex h-100 flex-direction--column">
-								<!-- Text -->
-								<p class="the-services-swiper__slide-overlay-text typo--body typo--medium ">
-									<?php echo $dummy_card['text'] ?>
-								</p>
+							<!-- Tags -->
+							<div class="mt-auto">
+								<!-- TagsCaption -->
+								<span class="is-block color--white-60 typo--body typo--medium mb-2xs">Full features:</span>
+								<div class="is-flex align-items--flex-end">
 
-								<div class="mt-auto">
-									<!-- TagsCaption -->
-									<span class="is-block color--white-60 typo--body typo--medium mb-2xs">Full features:</span>
-									<div class="is-flex align-items--flex-end">
-
-										<!-- TagsCloud -->
-										<div class="the-services-swiper__tags mr-2xs">
-											<?php
-											foreach ( $dummy_card['tags'] as $tag ) {
-												?>
-												<span class="typo--body typo--medium color--white is-rounded--s bg-color--white-10 py-4xs px-3xs">
-													<?php echo $tag ?>
-												</span>
-												<?php
-											}
-											?>
-										</div>
-
-										<!-- CloseBtn -->
-										<button class="the-services-swiper__slide-close reset-button-styles is-rounded--full mt-auto ml-auto flex-shrink--0"></button>
+									<!-- TagsCloud -->
+									<div class="the-services-swiper__tags mr-2xs">
+										<span class="typo--body typo--medium color--white is-rounded--s bg-color--white-10 py-4xs px-3xs">
+											op-eds
+										</span>
+										<span class="typo--body typo--medium color--white is-rounded--s bg-color--white-10 py-4xs px-3xs">
+											podcasts
+										</span>
+										<span class="typo--body typo--medium color--white is-rounded--s bg-color--white-10 py-4xs px-3xs">
+											expert quotes
+										</span>
+										<span class="typo--body typo--medium color--white is-rounded--s bg-color--white-10 py-4xs px-3xs">
+											data citations
+										</span>
+										<span class="typo--body typo--medium color--white is-rounded--s bg-color--white-10 py-4xs px-3xs">
+											commentaries
+										</span>
+										<span class="typo--body typo--medium color--white is-rounded--s bg-color--white-10 py-4xs px-3xs">
+											interviews
+										</span>
+										<span class="typo--body typo--medium color--white is-rounded--s bg-color--white-10 py-4xs px-3xs">
+											newsjacking
+										</span>
 									</div>
+
+									<!-- CloseBtn -->
+									<button class="the-services-swiper__slide-close reset-button-styles is-rounded--full mt-auto ml-auto flex-shrink--0 mr-xs mb-xs mr-m mr-m__desktop mb-m__desktop mr-xs__widescreen mr-xs_widescreen mr-m__fullhd mb-m__fullhd"></button>
 								</div>
 							</div>
 						</div>
 					</div>
-					<?php
-				}
-				?>
+				</div>
+
+				<!-- The Services Swiper Card 2 -->
+				<div class="swiper-slide is-flex flex-direction--column the-services-swiper__slide is-rounded--4xs is-clipped bg-color--white p-xs color--white p-m__desktop p-xs__widescreen p-m__fullhd">
+					<div class="the-services-swiper__slide-face is-flex h-100 flex-direction--column">
+
+						<!-- BgImage -->
+						<img width="320" height="526" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/the-services-slider-card-bg.webp' ?>" alt="Market Across SEO" class="the-services-swiper__slide-bg">
+
+						<!-- Title -->
+						<h3 class="typo--h1 typo--light mt-0 mb-2xs">
+							SEO
+						</h3>
+
+						<!-- Subtitle -->
+						<p class="typo--body-big my-0 overflow--hidden">
+							Lorem ipsum dolor sit amet consectetur.
+						</p>
+
+						<!-- OpenBtn -->
+						<button class="the-services-swiper__slide-open reset-button-styles is-rounded--full mt-auto ml-auto flex-shrink--0 mr-xs mb-xs mr-m mr-m__desktop mb-m__desktop mr-xs__widescreen mr-xs_widescreen mr-m__fullhd mb-m__fullhd"></button>
+					</div>
+					<!-- Overlay -->
+					<div class="the-services-swiper__slide-overlay p-xs p-m__desktop p-xs__widescreen p-m__fullhd">
+						<div class="is-flex h-100 flex-direction--column">
+
+							<!-- Text -->
+							<p class="the-services-swiper__slide-overlay-text typo--body typo--medium mt-0 mb-2xs">
+								Search Engine Optimization comes down to directing a high-intent audience from Google search or your LLM of choice, to your website.
+							</p>
+							<p class="the-services-swiper__slide-overlay-text typo--body typo--medium mt-0 mb-2xs">
+								SEO is a great way to expand your reach, to get in front potential users and decision makers that don’t know you; yet.
+							</p>
+							<p class="the-services-swiper__slide-overlay-text typo--body typo--medium mt-0 mb-m">
+								The way it works: someone searches in Google, or asks Grok, how to solve a specific problem they have. One of the results on the first page is a link to a blog post, or a product page on your site that gives the most relevant explanation for solving that problem. Voila.
+							</p>
+
+							<p class="typo--unbounded typo--tag-small is-uppercase typo--height-13">
+								Bottomline: we’ll get you ranking on high-intent, highly-relevant search terms and keywords.
+							</p>
+
+							<!-- CloseBtn -->
+							<div class="mt-auto">
+								<button class="the-services-swiper__slide-close reset-button-styles is-rounded--full mt-auto ml-auto flex-shrink--0 mr-xs mb-xs mr-m mr-m__desktop mb-m__desktop mr-xs__widescreen mr-xs_widescreen mr-m__fullhd mb-m__fullhd"></button>
+							</div>
+						</div>
+
+					</div>
+				</div>
+
+				<!-- The Services Swiper Card 3 -->
+				<div class="swiper-slide is-flex flex-direction--column the-services-swiper__slide is-rounded--4xs is-clipped bg-color--white p-xs color--white p-m__desktop p-xs__widescreen p-m__fullhd">
+					<div class="the-services-swiper__slide-face is-flex h-100 flex-direction--column">
+
+						<!-- BgImage -->
+						<img width="320" height="526" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/the-services-slider-card-bg.webp' ?>" alt="Market Across ADVISORY" class="the-services-swiper__slide-bg">
+
+						<!-- Title -->
+						<h3 class="typo--h1 typo--light mt-0 mb-2xs">
+							ADVISORY
+						</h3>
+
+						<!-- Subtitle -->
+						<p class="typo--body-big my-0 overflow--hidden">
+							Lorem ipsum dolor sit amet consectetur.
+						</p>
+
+						<!-- OpenBtn -->
+						<button class="the-services-swiper__slide-open reset-button-styles is-rounded--full mt-auto ml-auto flex-shrink--0 mr-xs mb-xs mr-m mr-m__desktop mb-m__desktop mr-xs__widescreen mr-xs_widescreen mr-m__fullhd mb-m__fullhd"></button>
+					</div>
+					<!-- Overlay -->
+					<div class="the-services-swiper__slide-overlay p-xs p-m__desktop p-xs__widescreen p-m__fullhd">
+						<div class="is-flex h-100 flex-direction--column">
+
+							<!-- Text -->
+							<p class="typo--unbounded typo--tag-small is-uppercase typo--height-13 mt-0">
+								For companies just starting out, or those in need of a new direction
+							</p>
+
+							<p class="typo--subtitle typo--semibold mt-0 mb-3xs">
+								Brand identity
+							</p>
+							<p class="the-services-swiper__slide-overlay-text typo--body typo--medium mt-0 mb-xs">
+								The ideas, beliefs, goals, and story that together define your brand in the larger context of the industry
+							</p>
+
+							<p class="typo--subtitle typo--semibold mt-0 mb-3xs">
+								Messaging bible
+							</p>
+							<p class="the-services-swiper__slide-overlay-text typo--body typo--medium mt-0 mb-xs">
+								Messaging clusters tailored to specific use cases, audience segments, and market verticals
+							</p>
+
+							<p class="typo--subtitle typo--semibold mt-0 mb-3xs">
+								Channel impact breakdown
+							</p>
+							<p class="the-services-swiper__slide-overlay-text typo--body typo--medium mt-0 mb-xs">
+								Mapping of relevant media and social channels, then creating “attack plan” for each
+							</p>
+
+							<p class="typo--subtitle typo--semibold mt-0 mb-3xs">
+								Comms strategy
+							</p>
+							<p class="the-services-swiper__slide-overlay-text typo--body typo--medium mt-0 mb-xs">
+								Putting it all together - a comprehensive G2M blueprint for injecting your brand, company and product to right conversations
+							</p>
+
+
+							<!-- CloseBtn -->
+							<div class="mt-auto">
+								<button class="the-services-swiper__slide-close reset-button-styles is-rounded--full mt-auto ml-auto flex-shrink--0 mr-xs mb-xs mr-m mr-m__desktop mb-m__desktop mr-xs__widescreen mr-xs_widescreen mr-m__fullhd mb-m__fullhd"></button>
+							</div>
+						</div>
+
+					</div>
+				</div>
 			</div>
 			<!-- Pagination -->
 			<div class="the-services-swiper__pagination mt-m is-flex justify-content--center justify-content--left__tablet align-items--center is-hidden-widescreen"></div>
 		</div>
 
+		<!-- CTA Btn -->
 		<div class="is-flex justify-content--center__tablet">
 			<a href="#" class="btn--primary is-full-mobile mt-0">Let’s talk</a>
 		</div>
