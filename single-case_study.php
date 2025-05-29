4d8e1e95051c2c259573cbded53a50ce084ba64e
Valery Pronin <pownmymail@gmail.com>
2025-05-29 16:14:42 +0300

    feat: init Case Page template

---
+++ single-case_study.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template file for single Case Studies post
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+get_header();
+
+wp_rig()->print_styles( 'wp-rig-content' );
+
+?>
+<main id="case-page" class="site-main">
+
+	<!-- Hero -->
+	<?php get_template_part('template-parts/sections/case-page-hero') ?>
+
+</main>
+<?php
+get_footer();
---
+++ template-parts/sections/case-page-hero.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Template part for displaying single Case Page Hero Section
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+?>
+<div class="page-hero-wrapper">
+	<section class="case-page-hero py-4xl color--white py-xl__tablet pb-4xl__desktop py-6xl__widescreen">
+		<div class="container">
+            <h2 class="typo--h2 color--deep-purple">Hello</h2>
+        </div>
+    </section>
+</div>
