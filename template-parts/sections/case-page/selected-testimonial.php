1345798ac91de417b1c75a221f71759dee46544f
Valery Pronin <pownmymail@gmail.com>
2025-06-04 22:43:51 +0300

    feat: add Case page Selected Testimonial acf fields

--- template-parts/sections/case-page/selected-testimonial.php
+++ template-parts/sections/case-page/selected-testimonial.php
@@ -6,6 +6,8 @@
  * @package wp_rig
  */
 
+$testimonial_post_id = get_field( 'case_selected_testimonial' )[0] ?? null;
+
 ?>
 
 <section id="selected-testimonial" class="selected-testimonial py-2xl">
@@ -27,7 +29,7 @@
 
 						<!-- Text -->
 						<p class="mt-0 mb-0 typo--body-big typo--h2__desktop">
-							The MarketAcross team have been amazing to work with. They play a core role in delivering Splinterlands' messaging globally. Their understanding of the industry and work ethic is unrivaled.
+							<?php echo get_field( 'testimonial_text', $testimonial_post_id ) ?>
 						</p>
 					</div>
 				</div>
@@ -38,15 +40,15 @@
 					<div class="is-flex align-items--center flex-direction--column__desktop align-items--flex-start__desktop mr-l__desktop">
 
 						<!-- Avatar -->
-						<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/person.png" alt="Person Name" class="selected-testimonial__avatar is-rounded--4xs mr-xs mr-0__desktop mb-xs__desktop">
+						<img src="<?php echo get_field( 'author_image', $testimonial_post_id )['url'] ?>" alt="<?php echo get_field( 'author_name', $testimonial_post_id ) ?>" class="selected-testimonial__avatar is-rounded--4xs mr-xs mr-0__desktop mb-xs__desktop">
 
 						<!-- About -->
 						<div class="">
 							<span class="is-block typo--body-big typo--medium mb-4xs typo--subtitle-big__desktop typo--regular__desktop">
-								Sandeep Nailwal
+								<?php echo get_field( 'author_name', $testimonial_post_id ) ?>
 							</span>
 							<span class="is-block typo--body-small color--gray typo--body__desktop typo--medium">
-								Co-founder & COO
+								<?php echo get_field( 'author_position', $testimonial_post_id ) ?>
 							</span>
 						</div>
 					</div>
